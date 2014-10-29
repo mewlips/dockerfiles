871118ac1a2868ceaa0f45f3d75102064914dc60
mewlips <mewlips@gmail.com>
2014-10-29 19:31:11 +0900

    remove android-kitkat-builder/{.gitignore,run.sh}

--- android-kitkat-builder/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$1"
-
-if [ -d "$DIR" ]; then
-    docker run --rm -i -t --privileged -v "$DIR:$DIR" -u $USER -w "$DIR" android-kitkat-build 
-else
-    docker run --rm -i -t android-kitkat-build 
-fi
